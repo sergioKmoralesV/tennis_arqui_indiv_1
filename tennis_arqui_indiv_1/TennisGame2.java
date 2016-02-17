7cc6017b2c2a913e3e961266c62d020398690f78
sergioMoralesPercella <sergio.morales@percella.com>
2016-02-17 07:15:09 -0400

    Advantage and winner review

--- tennis_arqui_indiv_1/TennisGame2.java
+++ tennis_arqui_indiv_1/TennisGame2.java
@@ -27,10 +27,35 @@ public class TennisGame2 implements TennisGame
              score = getEquivalence(P1point)+"-All";
          }
     }
+    public void reviewForDeuce(){
+    	if (P1point==P2point && P1point>=3)
+            score = "Deuce";
+    }
+    public void reviewForWinner(){
+    	if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+        {
+            score = "Win for player1";
+        }
+        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        {
+            score = "Win for player2";
+        }
+    }
+    
+    public void reviewForAdvantagePoint(){
+        if (P1point > P2point && P2point >= 3)
+        {
+            score = "Advantage player1";
+        }
+        
+        if (P2point > P1point && P1point >= 3)
+        {
+            score = "Advantage player2";
+        }
+    }
     public String getScore(){
         reviewForEqualPoints();
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
+        reviewForDeuce();
         
         if (P1point > 0 && P2point==0)
         {
@@ -82,24 +107,8 @@ public class TennisGame2 implements TennisGame
             score = P1res + "-" + P2res;
         }
         
-        if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1";
-        }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
-        }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
-        }
+        reviewForAdvantagePoint();
+        reviewForWinner();
         return score;
     }
     
